b310029d5844ed5c3842ecb01fb39e0def7c172e
Shan Yu Lin <ineslin35@gmail.com>
2022-05-14 23:01:43 +0800

    chore: install Material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import Docs from './components/docs'
+
 function App() {
   return (
     <div className="App">
-
+      <Docs />
     </div>
   );
 }
---
+++ src/components/docs.js
@@ -0,0 +1,10 @@
+function Docs() {
+    return (
+        <div className="docs-main">
+            hello
+            <button className="add-docs">Add a Document</button>
+        </div>
+    )
+}
+
+export default Docs
