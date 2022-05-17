37e60d296636e557e8f221b5b35fdabd0978e6c4
Shan Yu Lin <ineslin35@gmail.com>
2022-05-17 19:25:42 +0800

    feat: setup the layout of the add-new-docs modal

--- src/components/docs.js
+++ src/components/docs.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
+import TheModal from "./modal";
+
 function Docs() {
-    return (
-        <div className="docs-main">
-            hello
-            <button className="add-docs">Add a Document</button>
-        </div>
-    )
+  const [isOpen, setOpen] = useState(false);
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
+
+  return (
+    <div className="docs-main">
+      <button onClick={openModal} className="add-docs">
+        Add a Document
+      </button>
+      <TheModal isOpen={isOpen} closeModal={closeModal} />
+    </div>
+  );
 }
 
-export default Docs
+export default Docs;
