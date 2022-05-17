dbc512727a264cc9595618d231e7b73fe74bb431
Shan Yu Lin <ineslin35@gmail.com>
2022-05-17 20:07:50 +0800

    feat: fetch the docs' datum in the firebase database

--- src/components/docs.js
+++ src/components/docs.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { addDoc, collection } from "firebase/firestore"
+import { useState, useEffect, useRef } from "react"
+import { addDoc, collection, onSnapshot } from "firebase/firestore"
 
 import TheModal from "./modal"
 
@@ -10,7 +10,16 @@ function Docs({ database }) {
 
     const [title, setTitle] = useState('')
 
+    const [docsData, setDocsDate] = useState([])
+
     const collectionRef = collection(database, 'docsData')
+    const getData = () => {
+        onSnapshot(collectionRef, (data) => {
+            setDocsDate(data.docs.map((doc) => {
+                return { ...doc.data(), id: doc.id }
+            }))
+        })
+    }
     const addData = () => {
         addDoc(collectionRef, {
             title: title
@@ -24,11 +33,27 @@ function Docs({ database }) {
             })
     }
 
+    const isMounted = useRef()
+    useEffect(() => {
+        if (isMounted.current) return
+        isMounted.current = true
+        getData()
+    }, [])
+
     return (
         <div className="docs-main">
             <button onClick={openModal} className="add-docs">
                 Add a Document
             </button>
+            <div className="grid-main">
+                {docsData.map((doc) => {
+                    return (
+                        <div className="grid-child">
+                            <p>{doc.title}</p>
+                        </div>
+                    )
+                })}
+            </div>
             <TheModal isOpen={isOpen} closeModal={closeModal} title={title} setTitle={setTitle} addData={addData} />
         </div>
     )
