7590fa0fac370cf27fea54275c2dc12c31959c5a
Shan Yu Lin <ineslin35@gmail.com>
2022-05-19 20:17:03 +0800

    feat: store the content of the doc in the Firebase database

--- src/components/editDoc.js
+++ src/components/editDoc.js
@@ -1,10 +1,39 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { doc, collection, updateDoc } from "firebase/firestore"
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
-function EditDoc() {
+function EditDoc({ database }) {
     let params = useParams()
+    const [docsDesc, setDocsDesc] = useState('')
+    useEffect(() => {
+        console.log("hello")
+        const updateDocsData = setTimeout(() => {
+            console.log("hell0000000o")
+            const document = doc(collectionRef, params.id)
+            updateDoc(document, {
+                docsDesc: docsDesc
+            }).then(() => {
+            })
+            .catch(() => {
+            })
+        }, 1000)
+
+        return () => clearTimeout(updateDocsData)
+    }, [docsDesc])
+
+    const getQuillData = (value) => {
+        setDocsDesc(value)
+    }
+
+    const collectionRef = collection(database, 'docsData')
 
     return (
-        <div>EditDocs</div>
+        <div>
+            <h1>Editing</h1>
+            <ReactQuill value={docsDesc} onChange={getQuillData} />
+        </div>
     )
 }
 
