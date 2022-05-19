b44a6e1061b600c303caec63f0692637344b2396
Shan Yu Lin <ineslin35@gmail.com>
2022-05-19 20:41:32 +0800

    feat: fetch the doc's data from the Firebase database

--- src/components/editDoc.js
+++ src/components/editDoc.js
@@ -1,16 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
-import { doc, collection, updateDoc } from "firebase/firestore"
+import { doc, collection, updateDoc, onSnapshot } from "firebase/firestore"
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 
 function EditDoc({ database }) {
     let params = useParams()
+    const [docsTitle, setDocsTitle] = useState('')
     const [docsDesc, setDocsDesc] = useState('')
     useEffect(() => {
-        console.log("hello")
         const updateDocsData = setTimeout(() => {
-            console.log("hell0000000o")
             const document = doc(collectionRef, params.id)
             updateDoc(document, {
                 docsDesc: docsDesc
@@ -23,15 +22,32 @@ function EditDoc({ database }) {
         return () => clearTimeout(updateDocsData)
     }, [docsDesc])
 
+    const isMounted = useRef()
+    useEffect(() => {
+        if (isMounted.current) return
+
+        isMounted.current = true
+        getData()
+    }, [])
+
     const getQuillData = (value) => {
         setDocsDesc(value)
     }
 
     const collectionRef = collection(database, 'docsData')
 
+    const getData = () => {
+        const document = doc(collectionRef, params.id)
+        onSnapshot(document, (docs) => {
+            const docsData = docs.data()
+            setDocsTitle(docsData.title)
+            setDocsDesc(docsData.docsDesc)
+        })
+    }
+
     return (
         <div>
-            <h1>Editing</h1>
+            <h1>{docsTitle}</h1>
             <ReactQuill value={docsDesc} onChange={getQuillData} />
         </div>
     )
